5ef83d80368966230bea15ab906652184fba48b5
Mukundi <mukundiwitness18@gmail.com>
2025-06-30 22:55:31 +0300

    portfolio

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,1 +1,23 @@
 // Create your About component here
+import IMG from "../assets/Detective.png";
+
+const About = () => {
+    return(
+        <div id="about" className="about">
+            <h1 className="about-heading">About Me</h1>
+            <div className="about-info">
+                <p className="about-desc">Hello I am a second year student at Strathmore University studying Computer Science
+                    and I am passionate about software development, particularly in the areas of web and mobile applications. 
+                    I enjoy solving complex problems and continuously learning new technologies to enhance my skills.
+                </p>
+                <div className="about-img">
+                    <div className="about-img-wrapper">
+                        <img src={IMG} alt="Detective"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About;
--- portfolio/src/components/Body.js
+++ portfolio/src/components/Body.js
@@ -1,1 +1,27 @@
 // Create your Body component here
+import {FaGithub, FaLinkedin} from "react-icons/fa";
+import Avatar from "../assets/Software.png";
+
+const Body = () => {
+    return (
+        <div id="body" className="body">
+            <div className="body-container">
+                <div className="body-profile">
+                    <img className="body-img" alt="avatar" src={Avatar}/>
+
+                    <div className="body-content">
+                        <div className="body-headline">Mukundi Witness Chingwena</div>
+                        <div className="body-text">Student</div>
+                    </div>
+
+                    <div className="body-icons">
+                        <a href="https://www.educative.io/" target="_blank" rel= "noreferrer" className="icon-link"><i><FaGithub/></i></a>
+                        <a href="https://www.educative.io/" target = "_blank" rel="noreferrer" className="icon-link"><i><FaLinkedin/></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Body;
